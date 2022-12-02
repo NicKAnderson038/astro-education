ae7b2a1c16b71f476b5e355aa688ae942b69c396
NicKAnderson038 <nick7anderson@protonmail.com>
2022-12-01 21:25:42 -0800

    include lock file

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'nickanderson038.github.io',
-  // base: '/astro-education',
+  site: 'https://nickanderson038.github.io',
+  base: '/astro-education',
 });
